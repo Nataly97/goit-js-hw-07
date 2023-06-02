e0464b79609f1b26dc4f43477e4f9817c6c4cb03
Nataly Martínez <nataly.97.11.m@gmail.com>
2023-06-01 21:43:13 -0500

    Solución

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+const gallery = document.querySelector("ul.gallery");
+let galleryMarkup = '';
+
+galleryItems.forEach((img) => {
+    const imges =
+        `<li class="gallery__item">
+         <a class="gallery__link" href="${img.original}">
+            <img
+                class="gallery__image"
+                src="${img.preview}"
+                data-source="${img.original}"
+                alt="Image description"
+            />
+        </a>    
+    </li>`;
+
+    galleryMarkup += imges;
+
+});
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+
+const galleryLinks = document.querySelectorAll('.gallery__link');
+galleryLinks.forEach((link) => {
+    link.onclick = (event) => {
+        event.preventDefault();
+        const url = event.currentTarget;
+        basicLightbox.create(`
+                <img width="1400" height="900" src="${url.href}">
+            `).show();
+    };
+});
+
+// const close = document.querySelector(".gallery__item");
+
+gallery.addEventListener("keydown", (event) => {
+    if (event.code === 'Escape') {
+        console.log(event);
+        basicLightbox.close();
+        // basicLightbox.close(() => console.log('la caja de luz ya no es visible'))
+    }
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,38 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector("ul.gallery");
+let galleryMarkup = '';
+
+galleryItems.forEach((img) => {
+    const imges =
+        `<li class="gallery__item">
+            <a class="gallery__link" href="${img.original}">
+            <img class="gallery__image" src="${img.preview}" alt="${img.description}" />
+            </a>
+     </li>`;
+
+    galleryMarkup += imges;
+
+});
+
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+let lightbox = new SimpleLightbox('.gallery__item a', {
+    captionsData: "alt",
+    captionDelay: 250,
+});
+
+
+
+// const galleryDescrip = document.querySelectorAll('.gallery__image');
+// galleryDescrip.forEach((link) => {
+//     link.onclick = (event) => {
+//         event.preventDefault();
+
+      
+
+//     };
+// });
+
